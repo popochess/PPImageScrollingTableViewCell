be856f32282992e04329d91173ea89b6703e02d4
Popochess Lee <popochess@gmail.com>
2013-08-14 19:58:38 +0800

    Add PPImageScrollingTableViewCell

---
+++ PPImageScrollingTableViewControllerDemo/PPImageScrollingTableViewCell/PPImageCollectionModel.h
@@ -0,0 +1,16 @@
+//
+//  PPImageCollectionModel.h
+//  PPImageScrollingTableViewControllerDemo
+//
+//  Created by popochess on 13/8/13.
+//  Copyright (c) 2013年 popochess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPImageCollectionModel : NSObject
+
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSString *title;
+
+@end
---
+++ PPImageScrollingTableViewControllerDemo/PPImageScrollingTableViewCell/PPImageCollectionModel.m
@@ -0,0 +1,26 @@
+//
+//  PPImageCollectionModel.m
+//  PPImageScrollingTableViewControllerDemo
+//
+//  Created by popochess on 13/8/13.
+//  Copyright (c) 2013年 popochess. All rights reserved.
+//
+
+#import "PPImageCollectionModel.h"
+
+@interface PPImageCollectionModel ()
+
+@end
+
+@implementation PPImageCollectionModel
+
+- (void)setImage:(UIImage *)image{
+
+}
+
+- (void)setTitle:(NSString *)title{
+
+}
+
+
+@end
